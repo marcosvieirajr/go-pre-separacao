0a6f8eda71b08e4788d822ee5fe6ae80fc6682d5
Marcos Vieira Jr <13818801+marcosvieirajr@users.noreply.github.com>
2022-03-12 22:59:54 -0300

    feat: add grpc main file

---
+++ cmd/grpc/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/viavarejo-internal/pre-separacao-api/kit/config"
+	logger "github.com/viavarejo-internal/pre-separacao-api/kit/log"
+)
+
+var (
+	cfg config.Config
+	log *logrus.Entry
+)
+
+func init() {
+	cfg = config.Load()
+	log = logger.Config(cfg.Environment, cfg.LogFormat, cfg.LogLevel).
+		WithFields(logrus.Fields{
+			"app": "pre-separacao",
+		})
+}
+
+func main() {
+	log.Info("Hello, gRPC World!")
+	log.Info(cfg.Environment)
+}
