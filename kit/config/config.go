79dd92ad46f7bf801b0742cc84884905337c8646
Marcos Vieira Jr <13818801+marcosvieirajr@users.noreply.github.com>
2022-03-13 21:43:34 -0300

    refactor: update kit config

--- kit/config/config.go
+++ kit/config/config.go
@@ -8,60 +8,63 @@ import (
 	"github.com/joho/godotenv"
 )
 
-type Config struct {
-	Environment string
-	Host        string
-	Port        string
-	AllowedCORS string
-	LogFormat   string
-	LogLevel    string
-	Wait        time.Duration
+// default values
+var (
+	ENVIRONMENT  = "dev"
+	HOST_NAME    = ""
+	HOST_PORT    = "8080"
+	ALLOWED_CORS = ""
+	LOG_LEVEL    = "info"
+	// LOG_FORMAT   = "text"
+	GRACEFUL_TIMEOUT = time.Second * 15
+)
+
+func Load() {
+	// load env variables
+	loadEnvs()
+
+	// flags take precedence over env vars
+	loadFlags()
 }
 
-func Load() Config {
-
+func loadEnvs() {
+	// Load dev env vars from .env file. It's important to note that
+	// it WILL NOT OVERRIDE an env variable that already exists
 	godotenv.Load(".env")
 
-	cfg := Config{
-		Environment: "dev",
-		AllowedCORS: os.Getenv("ALLOWED_CORS"),
-		Host:        os.Getenv("HOST_NAME"),
-		Port:        "8080",
-		LogLevel:    "info",
-		LogFormat:   "text",
-		Wait:        time.Second * 15,
-	}
-
-	flag.StringVar(&cfg.AllowedCORS, "cors", cfg.AllowedCORS, "Cross-Origin Resource Sharing")
-	flag.StringVar(&cfg.Host, "host", cfg.Host, "HTTP service host name")
+	HOST_NAME = os.Getenv("HOST_NAME")
+	ALLOWED_CORS = os.Getenv("ALLOWED_CORS")
 
 	if env, ok := os.LookupEnv("ENVIRONMENT"); ok && env != "" {
-		cfg.Environment = env
+		ENVIRONMENT = env
 	}
-	flag.StringVar(&cfg.Environment, "environment", cfg.Environment, "Runtime environment [dev, hlg, stg, prd]. Default: dev")
 
 	if env, ok := os.LookupEnv("HOST_PORT"); ok && env != "" {
-		cfg.Port = env
+		HOST_PORT = env
 	}
-	flag.StringVar(&cfg.Port, "port", cfg.Port, "HTTP service port. Default: 8080")
 
 	if env, ok := os.LookupEnv("LOG_LEVEL"); ok && env != "" {
-		cfg.LogLevel = env
+		LOG_LEVEL = env
 	}
-	flag.StringVar(&cfg.LogLevel, "log-level", cfg.LogLevel, "Log output level for the server [debug, info, trace]. Default: info")
 
-	if env, ok := os.LookupEnv("LOG_FORMAT"); ok && env != "" {
-		cfg.LogFormat = env
-	}
-	flag.StringVar(&cfg.LogFormat, "log-format", cfg.LogFormat, "Log output format [text, json]. Default: text")
+	// if env, ok := os.LookupEnv("LOG_FORMAT"); ok && env != "" {
+	// 	LOG_FORMAT = env
+	// }
 
 	if env, ok := os.LookupEnv("GRACEFUL_TIMEOUT"); ok && env != "" {
-		if w, err := time.ParseDuration(env); err == nil {
-			cfg.Wait = w
+		if wait, err := time.ParseDuration(env); err == nil {
+			GRACEFUL_TIMEOUT = wait
 		}
 	}
-	flag.DurationVar(&cfg.Wait, "graceful-timeout", cfg.Wait, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m. Default: 15s")
+}
 
+func loadFlags() {
+	flag.StringVar(&ALLOWED_CORS, "cors", ALLOWED_CORS, "Cross-Origin Resource Sharing")
+	flag.StringVar(&HOST_NAME, "host", HOST_NAME, "HTTP service host name")
+	flag.StringVar(&ENVIRONMENT, "environment", ENVIRONMENT, "Runtime environment [dev, hlg, stg, prod]. Default: dev")
+	flag.StringVar(&HOST_PORT, "port", HOST_PORT, "HTTP service port. Default: 8080")
+	flag.StringVar(&LOG_LEVEL, "log-level", LOG_LEVEL, "Log output level for the server [debug, info, trace]. Default: info")
+	// flag.StringVar(&LOG_FORMAT, "log-format", LOG_FORMAT, "Log output format [text, json]. Default: text")
+	flag.DurationVar(&GRACEFUL_TIMEOUT, "wait", GRACEFUL_TIMEOUT, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m. Default: 15s")
 	flag.Parse()
-	return cfg
 }
