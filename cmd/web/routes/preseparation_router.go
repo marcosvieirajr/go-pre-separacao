a01eb88c297662dd039fc3ca4fba3ff5d9d2f2e6
Marcos Vieira Jr <13818801+marcosvieirajr@users.noreply.github.com>
2022-03-29 23:57:41 -0300

    refactor: remove comented lines

--- cmd/web/routes/preseparation_router.go
+++ cmd/web/routes/preseparation_router.go
@@ -9,16 +9,12 @@ func MakeDocumentRoutes(r *gin.Engine, h *handlers.Document, m ...gin.HandlerFun
 	v1Auth := r.Group("/v1", m...)
 	{
 		v1Auth.GET("/pre-separacoes", h.ListAll())
-		// v1Auth.GET("/pre-separacoes/:filial/:document", h.ListOne())
 		v1Auth.PUT("/pre-separacoes/alterar-situacao", h.ChangeSituation())
-		// v1Auth.PUT("/pre-separacoes/:filial/:document/:situation", h.ChangeSituationPDV())
 	}
 
 	v1 := r.Group("/v1")
 	{
-		// v1.GET("/pre-separacoes", h.ListAll())
 		v1.GET("/pre-separacoes/:filial/:document", h.ListOne())
-		// v1.PUT("/pre-separacoes/alterar-situacao", h.ChangeSituation())
 		v1.PUT("/pre-separacoes/:filial/:document/:situation", h.ChangeSituationPDV())
 	}
 }
